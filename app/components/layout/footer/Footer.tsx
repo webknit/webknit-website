1b2b1ebb48040626e75f3db25df0c1c881b4ffd3
Shane Prendergast <contact@webknit.co.uk>
2022-12-02 20:39:23 +0000

    Update Footer.tsx

--- app/components/layout/footer/Footer.tsx
+++ app/components/layout/footer/Footer.tsx
@@ -38,7 +38,7 @@ export function Footer() {
           </a>
 
           <p className="c-footer__text u-mb-0">
-            © Webknit 2022
+            &copy; Webknit 2022
             <br></br>Please don’t copy, it took me ages!
           </p>
         </div>
