3ffafb9f02327ed81a9ddd94a86bf09968bad714
Shane Prendergast <contact@webknit.co.uk>
2022-12-02 21:01:08 +0000

    Update Footer.tsx

--- app/components/layout/footer/Footer.tsx
+++ app/components/layout/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Section } from "../section/Section";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 export function Footer() {
   return (
@@ -38,7 +39,7 @@ export function Footer() {
           </a>
 
           <p className="c-footer__text u-mb-0">
-            © Webknit {new Date().getFullYear()}
+            © Webknit 2022
             <span className="block">Please don't copy, it took me ages!</span>
           </p>
         </div>
